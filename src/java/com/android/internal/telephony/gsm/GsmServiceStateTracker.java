d455429b7266074b03aaa538a23356079e435339
Robert Greenwalt <rgreenwalt@google.com>
2013-12-19 16:03:03 -0800

    Fix isSameNamedOperatorConsideredRoaming [DO NOT MERGE]
    Does a loop checking data based on your current network,
    but exits out based on the result of the first check rather
    than doing the rest of the checks.
    
    Bug: 11408729
    Bug: 12121959
    Change-Id: I37f98682702ec19467f374d64a23e15fb87e9697

--- src/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
+++ src/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java
@@ -1365,12 +1365,14 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
         String[] numericArray = mPhone.getContext().getResources().getStringArray(
                     com.android.internal.R.array.config_operatorConsideredNonRoaming);
 
-        if (numericArray.length == 0 || operatorNumeric == null)
+        if (numericArray.length == 0 || operatorNumeric == null) {
             return false;
+        }
 
         for (String numeric : numericArray) {
-            if (operatorNumeric.startsWith(numeric))
+            if (operatorNumeric.startsWith(numeric)) {
                 return true;
+            }
         }
         return false;
     }
@@ -1380,14 +1382,14 @@ final class GsmServiceStateTracker extends ServiceStateTracker {
         String[] numericArray = mPhone.getContext().getResources().getStringArray(
                     com.android.internal.R.array.config_sameNamedOperatorConsideredRoaming);
 
-        if (numericArray.length == 0 || operatorNumeric == null)
+        if (numericArray.length == 0 || operatorNumeric == null) {
             return false;
+        }
 
         for (String numeric : numericArray) {
-            if (operatorNumeric.startsWith(numeric))
+            if (operatorNumeric.startsWith(numeric)) {
                 return true;
-            else
-                return false;
+            }
         }
         return false;
     }
