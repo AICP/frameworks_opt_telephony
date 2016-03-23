d027c3bad4b4bcafc02992c2e9b6ea8c41d69fa6
Vatsal <koradiavatsal@gmail.com>
2016-03-24 00:08:17 +0530

    Revert "Fix the invalid number when the one on the other side hangs up"
    This reverts commit 55d196f014db2296910e54925624efbb37f3c7ec.
    
    Change-Id: I3082508e988f2923430b50ec6a831ceff6938516

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -3549,16 +3549,8 @@ public class RIL extends BaseCommands implements CommandsInterface {
 
     protected Object
     responseFailCause(Parcel p) {
-        int numInts;
-        int response[];
-
-        numInts = p.readInt();
-        response = new int[numInts];
-        for (int i = 0 ; i < numInts ; i++) {
-            response[i] = p.readInt();
-        }
         LastCallFailCause failCause = new LastCallFailCause();
-        failCause.causeCode = response[0];
+        failCause.causeCode = p.readInt();
         if (p.dataAvail() > 0) {
           failCause.vendorCause = p.readString();
         }
