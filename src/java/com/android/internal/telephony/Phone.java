6a704b3ec9728e98def7659fd5916e04246a745b
Yashdev Singh <yashdevs@codeaurora.org>
2016-05-03 16:13:35 -0700

    Telephony: Enable data call on CSIM.
    - Let DCT get mcc/mnc from phone object.
    - GsmCdmaPhone would return mcc/mnc as follow...
        - If phoneType is GSM then mcc/mnc from SimRecords
        - If phoneType is CdmaLte then
            If CdmaSubscriptionSource is NV
                mcc/mnc from ro.cdma.home.operator.numeric
            Else
                If SimRecords present then mcc/mnc from this recored.
                Else mcc/mnc from RuimRecords.
    
    - ApnSettings UI would always display list of APNs based on mcc/mnc of GsmCdmaPhone.
        - CDMA operators having LTE
            - APN UI would list LTE apn profile since mcc/mnc is from SimRecords.
        - Legacy CDMA operators
            - APN UI would be empty since such simcard would not have Sim/Usim app.
            - OEM must add profile in apns-conf.xml for such operators.
    
    Change-Id: Ib8fd65eb8b18661d76a71ee24f1e732fb467d5c4

--- src/java/com/android/internal/telephony/Phone.java
+++ src/java/com/android/internal/telephony/Phone.java
@@ -3440,6 +3440,10 @@ public abstract class Phone extends Handler implements PhoneInternalInterface {
     public void cancelUSSD() {
     }
 
+    public String getOperatorNumeric() {
+        return "";
+    }
+
     /**
      * Set boolean broadcastEmergencyCallStateChanges
      */
